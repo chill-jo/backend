784ac27dd226fffa10dc2df2ed741476902f12f5
An-jihyun <anjh322@gmail.com>
2025-07-21 05:46:32 +0900

    feat: Add basic SecurityConfig - 보안 설정 기본 구성 추가

---
+++ src/main/java/com/example/surveyapp/global/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.surveyapp.global.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(it -> it.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
