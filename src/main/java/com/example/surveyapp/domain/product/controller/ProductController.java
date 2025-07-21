89c1df3492d1e4e3c924fa78eadd9bb72422ce98
handokei <ehgks994@naver.com>
2025-07-21 12:59:33 +0900

    임시 커밋: merge 전 작업 백업

---
+++ src/main/java/com/example/surveyapp/domain/product/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.example.surveyapp.domain.product.controller;
+
+import com.example.surveyapp.domain.ProductService;
+import com.example.surveyapp.domain.product.controller.dto.ProductCreateRequestDto;
+import com.example.surveyapp.domain.product.controller.dto.ProductCreateResponseDto;
+import com.example.surveyapp.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<ProductCreateResponseDto>> create(@RequestBody ProductCreateRequestDto dto) {
+
+    }
+}
