427427b4e46e85fba6849a1656af0fd6a2e0041b
kimminsung <shiwing15@naver.com>
2025-07-22 21:33:43 +0900

    chore: MapStruct 의존성 추가

---
+++ src/main/java/com/example/surveyapp/domain/survey/domain/model/entity/SurveyStatus.java
@@ -0,0 +1,26 @@
+package com.example.surveyapp.domain.survey.domain.model.entity;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+public enum SurveyStatus {
+    NOT_STARTED("시작 전"),
+    IN_PROGRESS("진행 중"),
+    PAUSED("일시 중지"),
+    DONE("종료");
+
+    private final String status;
+
+    SurveyStatus(String status){
+        this.status = status;
+    }
+
+    public static SurveyStatus of(String input){
+        return Arrays.stream(SurveyStatus.values())
+                .filter(s -> s.name().equalsIgnoreCase(input))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("설문 상태 오류"));
+    }
+}
