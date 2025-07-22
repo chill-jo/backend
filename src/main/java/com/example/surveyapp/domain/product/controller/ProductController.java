80919599626fdde90936ae2b1651f6f338b5a028
handokei <ehgks994@naver.com>
2025-07-22 20:00:08 +0900

    기본 클래스 생성

---
+++ src/main/java/com/example/surveyapp/domain/product/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.example.surveyapp.domain.product.controller;
+
+import com.example.surveyapp.domain.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+}
---
+++ src/main/java/com/example/surveyapp/domain/product/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.example.surveyapp.domain.product.service;
+
+import com.example.surveyapp.domain.product.model.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepepsitory;
+}
