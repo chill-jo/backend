7af3467972335ac0f562559c255d16ea0e193672
kimminsung <shiwing15@naver.com>
2025-07-22 10:39:31 +0900

    feat: question entity 구현

--- src/main/java/com/example/surveyapp/domain/survey/entity/Question.java
+++ src/main/java/com/example/surveyapp/domain/survey/entity/Question.java
@@ -1,6 +1,31 @@
 package com.example.surveyapp.domain.survey.entity;
 
 import com.example.surveyapp.global.response.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Question extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long number;
+
+    private String content;
+
+    private QuestionType type;
+
+    public void updateQuestion(){
+
+    }
 }
---
+++ src/main/java/com/example/surveyapp/domain/survey/entity/QuestionType.java
@@ -0,0 +1,5 @@
+package com.example.surveyapp.domain.survey.entity;
+
+public enum QuestionType {
+    SINGLE_CHOICE, MULTIPLE_CHOICE, SUBJECTIVE
+}
