aeeb12c7b595bae904e0b40d83dd2475a7898594
kimminsung <shiwing15@naver.com>
2025-07-22 10:44:27 +0900

    feat: option entity 구현

--- src/main/java/com/example/surveyapp/domain/survey/entity/Option.java
+++ src/main/java/com/example/surveyapp/domain/survey/entity/Option.java
@@ -1,6 +1,31 @@
 package com.example.surveyapp.domain.survey.entity;
 
 import com.example.surveyapp.global.response.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "option")
 public class Option extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    private Long number;
+
+    private String content;
+
+    public void updateOption(){
+
+    }
 }
