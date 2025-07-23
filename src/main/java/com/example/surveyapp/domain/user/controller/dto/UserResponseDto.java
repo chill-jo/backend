48b05dd90a0c89a452085a380b71e35a7d16fddd
gsemily <120404242+gsemily@users.noreply.github.com>
2025-07-23 13:03:05 +0900

    feat: UserService, UserController 기능 완성 (#54)
    * feat: UserController, UserService, DTO 관련 클래스 추가
    
    * feat: DTO 내용 추가, UserController 내용 작성
    
    * feat: UserService, UserController 기능 완성
    - BaseEntity 필드명 수정
    - ErrorCode 회원 관련 예외 추가
    - UserResponseDto 내용 수정

---
+++ src/main/java/com/example/surveyapp/domain/user/controller/dto/UserResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.surveyapp.domain.user.controller.dto;
+
+import com.example.surveyapp.domain.user.domain.model.User;
+import com.example.surveyapp.domain.user.domain.model.UserRoleEnum;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UserResponseDto {
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private String nickname;
+}
