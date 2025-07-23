ad4f53be3004af34d82a4955f9f3d458d9c071bf
kimminsung <shiwing15@naver.com>
2025-07-23 17:39:23 +0900

    feat: 질문 생성 요청 dto 수정

--- src/main/java/com/example/surveyapp/domain/survey/controller/dto/request/QuestionCreateRequestDto.java
+++ src/main/java/com/example/surveyapp/domain/survey/controller/dto/request/QuestionCreateRequestDto.java
@@ -12,13 +12,13 @@ import org.hibernate.validator.constraints.Length;
 public class QuestionCreateRequestDto {
 
     @NotNull(message = "질문 번호는 필수입니다.")
-    private final Long number;
+    private Long number;
 
     @NotBlank(message = "질문 내용은 필수입니다.")
     @Length(min = 5, max = 255, message = "질문 내용은 5~255자 사이여야 합니다.")
-    private final String content;
+    private String content;
 
     @NotNull(message = "질문 유형은 필수입니다.")
-    private final QuestionType type;
+    private QuestionType type;
 
 }
