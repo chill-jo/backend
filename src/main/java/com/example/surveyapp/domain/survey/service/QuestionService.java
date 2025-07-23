eb694764ea7ad29204f3995d6eb665fad4284ce2
kimminsung <shiwing15@naver.com>
2025-07-23 14:45:08 +0900

    feat: 질문 crud 기능 구현 위해 기본 파일 생성

--- src/main/java/com/example/surveyapp/domain/survey/service/QuestionService.java
+++ src/main/java/com/example/surveyapp/domain/survey/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.example.surveyapp.domain.survey.service;
 
+import com.example.surveyapp.domain.survey.domain.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class QuestionService {
 
+    private final QuestionRepository questionRepository;
 
+    //설문 상태 확인하는거 만들어야할듯
+    //해당 설문 출제자인지 확인해야함
+
+    public void createQuestion(){
+
+    }
+
+    public void updateQuestion(){
+
+    }
+
+    public void deleteQuestion(){
+
+    }
 }
