a72ad8b7d395ca8a98ae1e604ed19d15506d98c3
HyungWo-o <dbwotns153@naver.com>
2025-07-23 20:34:22 +0900

    test: admin 전체 회원 조회, 단일 회원 조회, 예외 TC 작성

---
+++ src/test/java/com/example/surveyapp/domain/admin/service/AdminServiceTest.java
@@ -0,0 +1,86 @@
+package com.example.surveyapp.domain.admin.service;
+
+import com.example.surveyapp.domain.admin.controller.dto.UserDto;
+import com.example.surveyapp.domain.user.domain.model.User;
+import com.example.surveyapp.domain.user.domain.repository.UserRepository;
+import com.example.surveyapp.global.response.exception.CustomException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class AdminServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserDto userDto;
+
+    @Mock
+    private User user;
+
+    @InjectMocks
+    private AdminService adminService;
+
+    @Test
+    void 검색어를_조건으로_전체_회원을_조회한다() {
+
+        // given
+        String search = "test";
+        Pageable pageable = Pageable.unpaged();
+        Page<UserDto> userList = Page.empty();
+        when(userRepository.findAllBySearch(search, pageable)).thenReturn(userList);
+
+        // when
+        Page<UserDto> result = adminService.findUserList(search, pageable);
+
+        // then
+        Assertions.assertThat(userList).isEqualTo(result);
+
+    }
+
+
+    @Test
+    void 단일_회원을_조회한다() {
+
+        // given
+        Long userId = 1L;
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        // when
+        UserDto result = adminService.findUser(userId);
+
+        // then
+        Assertions.assertThat(userDto.getId()).isEqualTo(result.getId());
+
+    }
+
+
+    @Test
+    void 단일_회원을_조회를_실패한다() {
+
+        // given
+        Long userId = 1L;
+
+        // when & then
+        try {
+            adminService.findUser(userId);
+        } catch (CustomException e) {
+            Assertions.assertThatExceptionOfType(CustomException.class)
+            .isThrownBy(() -> { throw new CustomException(e.getErrorCode()); })
+                    .withMessageContaining("사용자를 찾을 수 없습니다.");
+        }
+
+    }
+}
