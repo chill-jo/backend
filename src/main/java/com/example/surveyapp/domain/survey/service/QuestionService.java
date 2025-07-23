153d2ef49ca079786ff1523ce1ddcd05639b0f53
kimminsung <shiwing15@naver.com>
2025-07-23 14:28:54 +0900

    feat: 질문 crud 기본 파일 생성

---
+++ src/main/java/com/example/surveyapp/domain/survey/service/QuestionService.java
@@ -0,0 +1,13 @@
+package com.example.surveyapp.domain.survey.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+
+
+}
