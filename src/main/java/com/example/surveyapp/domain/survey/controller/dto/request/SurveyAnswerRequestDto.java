a63c37d708376935c518faff9df26c6cf3ca2a72
HyungWo-o <68930751+HyungWo-o@users.noreply.github.com>
2025-07-24 22:22:15 +0900

    Issues/96 (#98)
    * feature: 설문 응답 Dto 생성
    
    * feature: 설문 응답 기능 구현
    controller, service 작성

---
+++ src/main/java/com/example/surveyapp/domain/survey/controller/dto/request/SurveyAnswerRequestDto.java
@@ -0,0 +1,14 @@
+package com.example.surveyapp.domain.survey.controller.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class SurveyAnswerRequestDto {
+
+    private List<QuestionAnswerRequestDto> answers;
+
+}
