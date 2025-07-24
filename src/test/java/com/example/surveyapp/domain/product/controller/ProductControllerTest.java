b7ba30ad7a0225ba85cf1d11b60ebba837627cd0
handokei <ehgks994@naver.com>
2025-07-24 16:57:14 +0900

    feat: 삭제 TC 구현

--- src/test/java/com/example/surveyapp/domain/product/controller/ProductControllerTest.java
+++ src/test/java/com/example/surveyapp/domain/product/controller/ProductControllerTest.java
@@ -74,7 +74,65 @@ class ProductControllerTest {
     }
 
     @Test
-    @DisplayName("상품을 전체 조회한다.")
+    @DisplayName("참여자 계정은 생성이 불가능하다")
+    @WithMockUser(username = "김도한", roles = "SURVEYEE")
+    void 상품_생성_참여자_계정은_불가능_하다() throws Exception{
+        // Given
+        String title = "상품 이름";
+        int price = 1800;
+        String content = "상품 설명";
+        Status status = Status.ON_SALE;
+
+        ProductCreateRequestDto requestDto = new ProductCreateRequestDto(title, content, price, status);
+        ProductCreateResponseDto responseDto = new ProductCreateResponseDto(1L,title, price, status);
+
+        when(productService.createProduct(any(ProductCreateRequestDto.class), eq(1L))).thenReturn(responseDto);
+        // When
+        //실행할 행동
+        ResultActions actions = mockMvc.perform(post("/api/products")
+                .param("userId","1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDto)));
+
+        // Then
+        //검증 사항
+        verify(productService,times(1)).createProduct(any(ProductCreateRequestDto.class),eq(1L));
+
+        actions.andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("출제자 계정은 생성이 불가능하다")
+    @WithMockUser(username = "김도한", roles = "SURVEYOR")
+    void 상품_생성_출제자_계정은_불가능_하다() throws Exception{
+        // Given
+        String title = "상품 이름";
+        int price = 1800;
+        String content = "상품 설명";
+        Status status = Status.ON_SALE;
+
+        ProductCreateRequestDto requestDto = new ProductCreateRequestDto(title, content, price, status);
+        ProductCreateResponseDto responseDto = new ProductCreateResponseDto(1L,title, price, status);
+
+        when(productService.createProduct(any(ProductCreateRequestDto.class), eq(1L))).thenReturn(responseDto);
+        // When
+        //실행할 행동
+        ResultActions actions = mockMvc.perform(post("/api/products")
+                .param("userId","1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDto)));
+
+        // Then
+        //검증 사항
+        verify(productService,times(1)).createProduct(any(ProductCreateRequestDto.class),eq(1L));
+
+        actions.andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("관리자 계정으로 상품을 전체 조회한다.")
     @WithMockUser(username = "김도한", roles = "ADMIN")
     void 상품_전체_조회를_한다() throws Exception {
         // Given
@@ -103,8 +161,66 @@ class ProductControllerTest {
     }
 
     @Test
+    @DisplayName("출제자 계정으로 상품 조회는 불가능하다.")
+    @WithMockUser(username = "김도한", roles = "SURVEYOR")
+    void 상품_전체_조회는_출제자계정은_불가능해야한다() throws Exception {
+        // Given
+        ProductResponseDto product1 = new ProductResponseDto(1L, "상품1", 2000, Status.ON_SALE);
+        ProductResponseDto product2 = new ProductResponseDto(2L, "상품2", 2500, Status.ON_SALE);
+        List<ProductResponseDto> productList = List.of(product1, product2);
+
+        when(productService.readAllProduct(0,10)).thenReturn(productList);
+
+        // When
+        //실행할 행동
+        ResultActions actions = mockMvc.perform(get("/api/products")
+                .param("page", "0")
+                .param("size", "10")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(productList)));
+        // Then
+        //검증 사항
+
+        verify(productService, times(1)).readAllProduct(0,10);
+
+        actions.andDo(print())
+                .andExpect(status().isForbidden());
+
+    }
+
+    @Test
+    @DisplayName("참여자 계정으로 상품 조회는 가능하다.")
+    @WithMockUser(username = "김도한", roles = "SURVEYEE")
+    void 상품_전체_조회_참여자계정은_가능해야한다() throws Exception {
+        // Given
+        ProductResponseDto product1 = new ProductResponseDto(1L, "상품1", 2000, Status.ON_SALE);
+        ProductResponseDto product2 = new ProductResponseDto(2L, "상품2", 2500, Status.ON_SALE);
+        List<ProductResponseDto> productList = List.of(product1, product2);
+
+        when(productService.readAllProduct(0,10)).thenReturn(productList);
+
+        // When
+        //실행할 행동
+        ResultActions actions = mockMvc.perform(get("/api/products")
+                .param("page", "0")
+                .param("size", "10")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(productList)));
+        // Then
+        //검증 사항
+
+        verify(productService, times(1)).readAllProduct(0,10);
+
+        actions.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data[0].title").value("상품1"))
+                .andExpect(jsonPath("$.data[1].title").value("상품2"));
+
+    }
+
+    @Test
     @DisplayName("상품을 한개 조회한다.")
-    @WithMockUser(username = "김도한", roles = "ADMIN")
+    @WithMockUser(username = "김도한", roles = "SURVEYEE")
     void 상품_한개를_조회한다() throws Exception{
         // Given
         //테스트 전제 조건 및 환경 설정
@@ -159,9 +275,62 @@ class ProductControllerTest {
     }
 
     @Test
+    @DisplayName("상품수정은 참여자가 불가능하다.")
+    @WithMockUser(username = "김도한", roles = "SURVEYEE")
+    void 상품수정_참여자는_불가능하다() throws Exception{
+        // Given
+        //테스트 전제 조건 및 환경 설정
+        Long productId = 1L;
+        ProductUpdateRequestDto requestDto = new ProductUpdateRequestDto("변경된 상품명", 2500, "변경된 상품설명", Status.ON_SALE);
+        ProductUpdateResponseDto responseDto = new ProductUpdateResponseDto(1L, "변경된 상품명", "변경된 상품설명:", 2500, Status.ON_SALE);
+
+        when(productService.updateProduct(eq(productId), any(ProductUpdateRequestDto.class))).thenReturn(responseDto);
+
+        // When
+        //실행할 행동
+        ResultActions actions = mockMvc.perform(patch("/api/products/{id}", productId)
+                .param("userId", "1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDto)));
+
+        // Then
+        //검증 사항
+        verify(productService,times(1))
+                .updateProduct(eq(productId), any(ProductUpdateRequestDto.class));
+        actions.andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
     @DisplayName("상품을 삭제 한다.")
     @WithMockUser(username = "김도한", roles = "ADMIN")
     void 상품을_삭제한다() throws Exception{
+
+        // Given
+        //테스트 전제 조건 및 환경 설정
+        Long productId = 1L;
+        Long userId = 1L;
+
+        ProductCreateRequestDto requestDto = new ProductCreateRequestDto("상품명", "상품설명", 2500, Status.ON_SALE);
+        ProductCreateResponseDto responseDto = new ProductCreateResponseDto(1L, "상품명", 2500, Status.ON_SALE);
+
+        doNothing().when(productService).deleteProduct(productId,userId);
+
+        // When
+        //실행할 행동
+        ResultActions actions = mockMvc.perform(delete("/api/products/{id}", productId)
+                .param("userId", "1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDto)));
+
+
+        // Then
+        //검증 사항
+        verify(productService, times(1)).deleteProduct(productId,userId);
+        actions.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isEmpty());
+
 
     }
 }
