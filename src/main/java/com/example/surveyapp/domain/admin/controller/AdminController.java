0257fd53fbf024faf04a0d52abc72ac320ec818d
HyungWo-o <68930751+HyungWo-o@users.noreply.github.com>
2025-07-25 20:17:09 +0900

    feature: 분류별 참여자 통계 조회 (기간) (#108)
    * feature: 분류별 참여자 통계 조회 (기간)
    controller, service, repository, dto 추가
    
    * 안쓰는 임포트 삭제

--- src/main/java/com/example/surveyapp/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/surveyapp/domain/admin/controller/AdminController.java
@@ -1,8 +1,8 @@
 package com.example.surveyapp.domain.admin.controller;
 
+import com.example.surveyapp.domain.admin.controller.dto.SurveyeeStatsDto;
 import com.example.surveyapp.domain.admin.controller.dto.UserDto;
 import com.example.surveyapp.domain.admin.service.AdminService;
-import com.example.surveyapp.domain.user.domain.model.User;
 import com.example.surveyapp.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -12,40 +12,47 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
 public class AdminController {
 
-    private final AdminService AdminService;
+    private final AdminService adminService;
 
-    /**
-     * 전체 회원 조회(검색)
-     *
-     * @param search 검색어 (필수 X)
-     * @return Page<UserDTO> 유저리스트 페이징
-     */
+    // 전체 회원 조회(검색)
     @GetMapping("/users")
-    public ResponseEntity<ApiResponse<Page<UserDto>>> findUserList(
+    public ResponseEntity<ApiResponse<Page<UserDto>>> getUserList(
             @RequestParam(required = false) String search,
             @PageableDefault(
                     size = 10,
                     sort = "id",
                     direction = Sort.Direction.ASC) Pageable pageable) {
 
-        return ResponseEntity.ok(ApiResponse.success("전체 회원 조회되었습니다.", AdminService.findUserList(search, pageable)));
+        return ResponseEntity.ok(ApiResponse.success("전체 회원 조회되었습니다.", adminService.getUserList(search, pageable)));
     }
 
-
-    /**
-     * 단일 회원 조회
-     *
-     * @param userId 유저 고유 식별자
-     * @return User 유저 정보
-     */
+    // 단일 회원 조회
     @GetMapping("/users/{userId}")
-    public ResponseEntity<ApiResponse<UserDto>> findUser(@PathVariable Long userId) {
-        return ResponseEntity.ok(ApiResponse.success("회원 조회되었습니다.", AdminService.findUser(userId)));
+    public ResponseEntity<ApiResponse<UserDto>> getUser(@PathVariable Long userId) {
+        return ResponseEntity.ok(ApiResponse.success("회원 조회되었습니다.", adminService.getUser(userId)));
+    }
+
+    // 분류별 참여자 통계 조회
+    @GetMapping("/stats/users")
+    public ResponseEntity<ApiResponse<List<SurveyeeStatsDto>>> getSurveyeeStats(
+            @RequestParam(required = false, defaultValue = "1999-01-01 00:00:00") String startDate,
+            @RequestParam(required = false, defaultValue = "2999-01-01 00:00:00") String endDate
+    ) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime startDateLocal = LocalDateTime.parse(startDate, formatter);
+        LocalDateTime endDateLocal = LocalDateTime.parse(endDate, formatter);
+
+        return ResponseEntity.ok(ApiResponse.success("분류별 참여자 통계 조회하였습니다.", adminService.getSurveyeeStats(startDateLocal, endDateLocal)));
+
     }
 }
