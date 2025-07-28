b50fb2a4c3f97657c91406843b9606ad60a1ede1
ahyoung <jr3537@naver.com>
2025-07-28 17:21:06 +0900

    feat(#119): 기본적인 예외처리
    - 충전, 지급, 교환 상황에 맞는 예외처리 추가
    
    close: #119

--- src/main/java/com/example/surveyapp/domain/point/domain/model/entity/Point.java
+++ src/main/java/com/example/surveyapp/domain/point/domain/model/entity/Point.java
@@ -2,6 +2,8 @@ package com.example.surveyapp.domain.point.domain.model.entity;
 
 import com.example.surveyapp.domain.user.domain.model.User;
 import com.example.surveyapp.global.config.entity.BaseEntity;
+import com.example.surveyapp.global.response.exception.CustomException;
+import com.example.surveyapp.global.response.exception.ErrorCode;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -38,14 +40,26 @@ public class Point  extends BaseEntity {
     }
 
     public void pointCharge(Long amount) {
+        if(amount==null || amount<=5000){
+            throw new CustomException(ErrorCode.POINT_INVALID_AMOUNT,"충전은 5000원부터 가능합니다.");
+        }
         this.pointBalance+=amount;
     }
 
     public void earn(Long amount) {
+        if(amount==null || amount<=0){
+            throw new CustomException(ErrorCode.POINT_EARN_FAILED,"포인트 지급에 실패했습니다.");
+        }
         this.pointBalance+=amount;
     }
 
     public void redeem(Long amount){
+        if(amount==null || amount<=0){
+            throw new CustomException(ErrorCode.POINT_MINIMUM_AMOUNT,"포인트가 존재하지 않습니다.");
+        }
+        if(pointBalance<amount){
+            throw new CustomException(ErrorCode.POINT_NOT_ENOUGH,"보유하신 포인트가 부족합니다.");
+        }
         this.pointBalance-=amount;
     }
 
