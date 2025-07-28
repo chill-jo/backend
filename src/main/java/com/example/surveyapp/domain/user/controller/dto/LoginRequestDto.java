2c0fd3970e9847308551151a07ecc742d66970bb
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2025-07-28 16:00:56 +0900

    feat: 로그인 및 회원 탈퇴 기능 구현 (#118)
    * Feat: 회원가입 기능 구현 - Register API 및 유효성 검증 추가
    
    * feat: 로그인 및 회원 탈퇴 기능 구현

---
+++ src/main/java/com/example/surveyapp/domain/user/controller/dto/LoginRequestDto.java
@@ -0,0 +1,15 @@
+package com.example.surveyapp.domain.user.controller.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class LoginRequestDto {
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
+}
