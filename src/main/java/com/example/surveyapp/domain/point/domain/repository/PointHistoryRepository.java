46b02a7c582f6da984e111bf8f53933720bf6ecc
아영 <67311685+youngee2@users.noreply.github.com>
2025-07-29 15:25:58 +0900

    feat(#93): point  내역 조회 및 page 공통 응답 생성 (#129)
    * refactor(#119): Status를 PointStatus로 변경
    
    - Product에 사용되는 status Enum과 구분하기 위해 수정
    - Status -> PointStatus
    
    close: #119
    
    * refactor(#119): Entity @AllArgsConstructor 제거
    
    - 직접 주입하면 안되는 식별자(id) 필드가 포함되어 있어 제거
    
    close: #119
    
    * refactor(#119): 정적 팩토리 메서드 패턴 + 빌더 패턴 적용
    
    - new 키워드로 직접 생성자를 생성하는 방식이 아닌 정적 팩토리 메서드 + 빌더 패턴 활용
    
    close: #119
    
    * feat(#119): Entity 연관관계 수정
    
    - Payment-PointHistory 연관관계 삭제
    - Point(1)-(N) PointHistory 연관관계 추가
    
    close: #119
    
    * feat(#119): 기본적인 예외처리
    
    - 충전, 지급, 교환 상황에 맞는 예외처리 추가
    
    close: #119
    
    * refactor(#119): 중복 예외 제거
    
    - PointService에 중복되는 예외 제거
    
    close: #119
    
    * refactor(#119): PointService 공통되는 로직을 메서드로 분리
    
    - 회원/포인트 조회가 중복되어 메서드로 분리 (getUser, getPoint)
    
    close: #119
    
    * feat(#93): point 내역 조회 Controller
    
    - 포인트 조회 controller 구현
    
    close: #93
    
    * feat(#93): point 내역 조회 repository
    
    - 포인트 조회 repository
    
    close: #93
    
    * feat(#93): point 내역 조회 repository
    
    - 포인트 내역 조회 repository
    
    close: #93
    
    * feat(#93): point 내역 조회 service
    
    - 포인트 내역 조회 service
    - readOnly=true 적용
    
    close: #93
    
    * feat(#93): point 예외 조건 수정
    
    - 포인트 예외 조건 수정
    
    close: #93
    
    * feat(#93): 회원가입 시 포인트 생성
    
    close: #93
    
    * feat(#93): Point RequestMapping 변경
    
    - /api -> /api/points
    close: #93
    
    * feat(#93): 페이징 공통 응답 생성
    
    close: #93
    
    * feat(#93): 페이징 공통 응답 적용
    
    close: #93
    
    * feat(#93): 페이징 공통 응답 수정 및 적용
    
    close: #93
    
    * feat(#93): 페이징 공통 응답 위치 변경
    
    - /global/config -> /global/response
    
    close: #93

--- src/main/java/com/example/surveyapp/domain/point/domain/repository/PointHistoryRepository.java
+++ src/main/java/com/example/surveyapp/domain/point/domain/repository/PointHistoryRepository.java
@@ -1,7 +1,13 @@
 package com.example.surveyapp.domain.point.domain.repository;
 
+import com.example.surveyapp.domain.point.domain.model.entity.Point;
 import com.example.surveyapp.domain.point.domain.model.entity.PointHistory;
+import com.example.surveyapp.domain.user.domain.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
+    Page<PointHistory> findByPoint_User(User user, Pageable pageable);
+
 }
