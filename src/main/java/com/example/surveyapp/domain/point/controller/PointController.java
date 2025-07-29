babe469531011d88bc50acba7ab92dbc5bc8215f
ahyoung <jr3537@naver.com>
2025-07-29 10:05:25 +0900

    feat(#93): point 내역 조회 Controller
    - 포인트 조회 controller 구현
    
    close: #93

--- src/main/java/com/example/surveyapp/domain/point/controller/PointController.java
+++ src/main/java/com/example/surveyapp/domain/point/controller/PointController.java
@@ -1,13 +1,17 @@
 package com.example.surveyapp.domain.point.controller;
 
 import com.example.surveyapp.domain.point.controller.dto.request.PointChargeRequestDto;
+import com.example.surveyapp.domain.point.controller.dto.response.PointHistoryResponseDto;
 import com.example.surveyapp.domain.point.service.PointService;
 import com.example.surveyapp.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping("/api")
@@ -17,11 +21,19 @@ public class PointController {
     private final PointService pointService;
 
     //포인트 충전
-    @PostMapping("/points")
+    @PostMapping("/points/charge")
     public ResponseEntity<ApiResponse<Void>> charge(@Valid @RequestBody PointChargeRequestDto dto,
                                                                      @RequestParam("userId") Long userId) {
         pointService.charge(userId,dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("결제가 되었습니다.",null));
+    }
+
+    //포인트 조회
+    @GetMapping("/points")
+    public ResponseEntity<ApiResponse<Page<PointHistoryResponseDto>>> getHistories(
+            @RequestParam("userId") Long userId, Pageable pageable){
+        Page<PointHistoryResponseDto> page = pointService.getHistories(userId,pageable);
+        return ResponseEntity.ok(ApiResponse.success("포인트 내역 조회 성공",page));
     }
 
 
