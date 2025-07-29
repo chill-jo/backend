9f60c1d252831a77132795a290b780faa9d95f98
ahyoung <jr3537@naver.com>
2025-07-29 12:13:31 +0900

    feat(#93): 페이징 공통 응답 생성
    close: #93

---
+++ src/main/java/com/example/surveyapp/global/config/PageResponse.java
@@ -0,0 +1,24 @@
+package com.example.surveyapp.global.config;
+
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+
+@Getter
+public class PageResponse<T> {
+    private List<T> content;
+    private Long totalElements;
+    private int totalPages;
+    private int size;
+    private int number;
+
+    public PageResponse(Page<T> page){
+        this.content = page.getContent();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.size = page.getSize();
+        this.number = page.getNumber();
+    }
+}
