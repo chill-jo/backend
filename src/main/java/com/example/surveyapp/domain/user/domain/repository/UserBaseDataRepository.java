da5574c14e24086a77b10124a73b11671fa0c4f6
HyungWo-o <68930751+HyungWo-o@users.noreply.github.com>
2025-07-30 15:49:03 +0900

    관리자 회원 조회, 통계 조회 1차 리팩터링 (#146)
    * feature: 참여자 기초 정보 등록 1차 커밋
    
    * feature: 참여자 기초 정보 C,R,U 기능 구현
    관련 Entity, Repository, Dto, Enum 신규 생성
    
    * refactor: 관리자 회원 조회, 통계 조회 기능 1차 리팩터링
    
    * refactor: merge 되면서 중복된 코드 수정

--- src/main/java/com/example/surveyapp/domain/user/domain/repository/UserBaseDataRepository.java
+++ src/main/java/com/example/surveyapp/domain/user/domain/repository/UserBaseDataRepository.java
@@ -4,7 +4,10 @@ import com.example.surveyapp.domain.user.domain.model.CategoryEnum;
 import com.example.surveyapp.domain.user.domain.model.User;
 import com.example.surveyapp.domain.user.domain.model.UserBaseData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,5 +16,19 @@ public interface UserBaseDataRepository extends JpaRepository<UserBaseData, Long
     Optional<UserBaseData> findByUserIdAndCategory(User userId, CategoryEnum category);
 
     List<UserBaseData> findAllByUserId(User userId);
+
+    @Query("""
+            SELECT COUNT(ubd)
+            FROM UserBaseData AS ubd
+            WHERE ubd.category = :category
+                    AND ubd.data = :data
+                    AND ubd.updatedAt >= :startDate
+                    AND ubd.updatedAt <= :endDate
+            """)
+    Long countByCategoryAndDataAndStartDateAndEndDate(
+            CategoryEnum category, Long data,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate
+    );
 
 }
